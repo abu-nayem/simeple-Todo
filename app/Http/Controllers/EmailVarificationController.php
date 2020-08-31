6c1d9546db1c38bcaaff9e1fe36315e60a220930
abu-nayem <70249427+abu-nayem@users.noreply.github.com>
2020-08-31 13:32:43 +0600

    Update EmailVarificationController.php

--- app/Http/Controllers/EmailVarificationController.php
+++ app/Http/Controllers/EmailVarificationController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\User;
 class EmailVarificationController extends Controller
 {
     
     public function checkmail(){
-    	return view('email.checkmail');
+    	if(Auth::user()->varified==0){
+    		return view('email.checkmail');
+    	}else{
+    		return redirect('/todos');
+    	}
     }
     public function verify($id){
     	$user=User::find($id);
